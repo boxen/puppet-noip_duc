46c90ceb6cf23fcd1f9bf52a1b0770f599f8a60b
MoOx <m@moox.fr>
2013-06-18 08:40:37 +0200

    Fix test

--- spec/classes/noip_duc_spec.rb
+++ spec/classes/noip_duc_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'noip_duc' do
   it do
     should contain_package('No-IP DUC').with({
-      :source   => 'https://d1ilhw0800yew8.cloudfront.net/client/Dropbox%202.0.5.dmg',
+      :source   => 'http://www.noip.com/client/mac/noip3.1.5.dmg',
       :provider => 'appdmg'
     })
   end
