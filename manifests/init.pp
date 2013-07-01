a0892cbf082d2b650cbb4ed76ed04ab2ea73e41f
MoOx <m@moox.fr>
2013-07-01 07:21:55 +0200

    Fix EULA issue

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@
 #   include noip_duc
 class noip_duc {
   package { 'NoIPDUC':
-    provider => 'appdmg',
+    provider => 'appdmg_eula',
     source   => 'http://www.noip.com/client/mac/noip3.1.5.dmg'
   }
 }
