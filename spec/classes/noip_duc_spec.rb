beacdd79fed1a27794438e144ba561cf5d0227fa
MoOx <m@moox.fr>
2013-07-01 07:31:19 +0200

    Fix travis CI build with appdmg_eula

--- spec/classes/noip_duc_spec.rb
+++ spec/classes/noip_duc_spec.rb
@@ -4,7 +4,7 @@ describe 'noip_duc' do
   it do
     should contain_package('NoIPDUC').with({
       :source   => 'http://www.noip.com/client/mac/noip3.1.5.dmg',
-      :provider => 'appdmg'
+      :provider => 'appdmg_eula'
     })
   end
 end
